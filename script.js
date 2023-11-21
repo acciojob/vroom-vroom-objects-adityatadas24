dbc1b4bd2d0938855ca6a367f21b93c4910123bb
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-11-21 11:27:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = function() {
+  return this.make + " " + this.model;
+};
+
+function SportsCar(make, model, topSpeed) {
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
+}
+
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+};
+
+var car = new Car("Toyota", "Camry");
+console.log(car.getMakeModel()); // "Toyota Camry"
+
+var sportsCar = new SportsCar("Ferrari", "488 GTB", 202);
+console.log(sportsCar.getMakeModel()); // "Ferrari 488 GTB"
+console.log(sportsCar.getTopSpeed());
 
 // Do not change the code below
 window.Car = Car;
